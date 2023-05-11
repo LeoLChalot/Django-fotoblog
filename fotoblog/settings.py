bc36d8ca87cf69f7c988da46e6414cd9bcd25a3a
LeoLChalot <larouchalotleo@gmail.com>
2023-05-11 10:37:46 +0200

    commit

--- fotoblog/settings.py
+++ fotoblog/settings.py
@@ -85,6 +85,7 @@ DATABASES = {
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
+AUTH_USER_MODEL = 'authentication.User'
 
 AUTH_PASSWORD_VALIDATORS = [
     {
